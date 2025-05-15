4da25dc075943146f6eb5dd61096273bd103cbeb
Red <vikel965@hotmail.com>
2025-05-14 23:33:30 -0400

    Refactored codebase to allow multiple AoC problems to be solved in the same repo.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using Advent_Of_Code_2025.AoC_Day_1;
+
+class AdvOfCode
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        DayOne dayOne = new DayOne();
+        dayOne.Run();
+    }
+}
