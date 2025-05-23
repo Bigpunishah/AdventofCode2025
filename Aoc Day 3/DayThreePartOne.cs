d738aab32900d513e0c54d1d94078ab75f527970
Red <vikel965@hotmail.com>
2025-05-22 21:58:23 -0400

    DayThreePartOne started :)

---
+++ Aoc Day 3/DayThreePartOne.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_Of_Code_2025.Aoc_Day_3
+{
+    public class DayThreePartOne
+    {
+        //Try to keep all concerns separated!
+        public void DayThreePartOneRun()
+        {
+
+        }
+
+        public string GetInputData()
+        {
+            try
+            {
+                string inputDat = File.ReadAllText("AoC Day Three\\InputData.txt");
+                return inputDat;
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message, "Error grabbing file from location - Make sure you edit properties to always copy! :)");
+            }
+            return String.Empty;
+        }
+    }
+}
