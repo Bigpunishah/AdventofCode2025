cbeeaeed56ee3427e334163b23191fbb33cc16a9
Red <vikel965@hotmail.com>
2025-05-26 23:22:26 -0400

    DayThreePartTwo setup started.

---
+++ Aoc Day 3/DayThreePartTwo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_Of_Code_2025.Aoc_Day_3
+{
+    class DayThreePartTwo : DayThreePartOne
+    {
+        //Do another Regex for the Do() & Dont() expressions 
+        //Use if statements within a loop?
+        public void DayThreePartTwoRun()
+        {
+
+        }
+
+
+    }
+}
