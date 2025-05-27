d8fcfed9aa6fadc95fd935f2af6a7548598a1f5a
Red <vikel965@hotmail.com>
2025-05-26 23:15:39 -0400

    DayThreePartOne Solved!

--- Aoc Day 3/DayThreePartOne.cs
+++ Aoc Day 3/DayThreePartOne.cs
@@ -21,23 +21,9 @@ namespace Advent_Of_Code_2025.Aoc_Day_3
         {
             string inputData = File.ReadAllText("AoC Day 3\\InputData.txt"); //Get data
             List<int> PatternedValuesList = GetPatternedValsList(inputData); //Get data as list in readable format for equation
+            int total = GetTotal(PatternedValuesList);
+            Console.WriteLine($"mul(x,y) total: {total}");
         }
-
-        public List<int> GetPatternedValsList(string data)
-        {
-            string regexpattern = @"mul+\(\d+,\d+\)"; 
-
-            foreach(Match match in Regex.Matches(data, regexpattern))
-            {
-                Console.WriteLine($"Matches: {match.Value}");
-                //Try to print one val then move from there.
-                
-
-            }
-            return new List<int>();
-        }
-
-
         public string GetInputData()
         {
             try
@@ -50,6 +36,33 @@ namespace Advent_Of_Code_2025.Aoc_Day_3
                 Console.WriteLine(ex.Message, "Error grabbing file from location - Make sure you edit properties to always copy! :)");
             }
             return String.Empty;
+        }
+
+        public List<int> GetPatternedValsList(string data)
+        {
+            string regexpattern = @"mul+\(\d+,\d+\)"; //Pattern to read data
+            List<int> values = [];
+
+            foreach(Match match in Regex.Matches(data, regexpattern))
+            {
+                int valOne = int.Parse(Regex.Match(match.Value, @"\d+").Value); //pattern for first val
+                int valTwo = int.Parse(Regex.Match(match.Value, @"(?<=,)\d+").Value); //pattern for second val (lookahead)
+                values.Add(valOne);
+                values.Add(valTwo);
+                //Console.WriteLine($"Matches: {valOne}, {valTwo}");
+            }
+            return values;
+        }
+
+        public int GetTotal(List<int> values)
+        {
+            int total = 0;
+            for (int i = 0; i < values.Count; i += 2)
+            {
+                total += values[i] * values[i + 1];
+                //Console.WriteLine("{0} {1}", values[i], values[i+1]);
+            }
+            return total;
         }
     }
 }
